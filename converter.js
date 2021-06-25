a296fb5d7e71d412e59f69271eca5dd5edbf4f0b
Nicolò Toscani <toscani.nicolo90@gmail.com>
2021-06-25 07:20:44 +0200

    Add files via upload

--- converter.js
+++ converter.js
@@ -15,6 +15,50 @@ module.exports = function(RED) {
             break;
             case "uint32":
                 msg.payload = msg.payload.buffer.readUInt32BE(0,4).toFixed(2);
+                
+                // Power Factor Total: convert powerfactor into 0 to 1 range 
+                if (msg.topic === "pFactorTot") {
+                    var payConv = Number(msg.payload);
+                    // Quad 1
+                    if(payConv >= 0 && payConv <= 1){
+                        msg.payload = payConv.toFixed(2);
+                        msg.util = {
+        
+                        loadType: "Inductive"
+                        }
+                        //node.send(msg);
+                     }
+                    // Quad 2
+                    if(payConv >= -1 && payConv <= 0){
+                        msg.payload = payConv.toFixed(2);
+                        msg.util = {
+       
+                        loadType: "Capacitive"
+                        }
+                        //node.send(msg);
+                     }
+                    // Quad 3
+                    if(payConv >= -2 && payConv <= -1){
+                        payConv = (-2) -payconv;
+                        msg.payload = payConv.toFixed(2);
+                        msg.util = {
+        
+                        loadType: "Inductive"
+                        }
+                        //node.send(msg);
+                    }
+                    // Quad 4
+                    if(payConv >= 1 && payConv <= 2){
+                        payConv = 2 - payConv;
+                        msg.payload = payConv.toFixed(2);
+                        msg.util = {
+        
+                        loadType: "Capacitive"
+                        }
+                      //node.send(msg);
+                    }
+                 }
+                
                 node.send(msg);
             break;
             case "int64":
@@ -26,48 +70,6 @@ module.exports = function(RED) {
                 node.send(msg);
         }
  
-// Power Factor Total: convert powerfactor into 0 to 1 range 
-if (msg.topic === "pFactorTot") {
-    var payConv = Number(msg.payload);
-    // Quad 1
-    if(payConv >= 0 && payConv <= 1){
-        msg.payload = payConv.toFixed(2);
-        msg.util = {
-        
-            loadType: "Inductive"
-        }
-        node.send(msg);
-    }
-    // Quad 2
-    if(payConv >= -1 && payConv <= 0){
-       msg.payload = payConv.toFixed(2);
-       msg.util = {
-       
-        loadType: "Capacitive"
-    }
-       node.send(msg);
-    }
-    // Quad 3
-    if(payConv >= -2 && payConv <= -1){
-        payConv = (-2) -payconv;
-        msg.payload = payConv.toFixed(2);
-        msg.util = {
-        
-            loadType: "Inductive"
-        }
-        node.send(msg);
-     }
-    // Quad 4
-    if(payConv >= 1 && payConv <= 2){
-        payConv = 2 - payConv;
-        msg.payload = payConv.toFixed(2);
-        msg.util = {
-        
-            loadType: "Capacitive"
-        }
-        node.send(msg);
-    }
-}
         });
     }
     RED.nodes.registerType("converter",converterNode);
