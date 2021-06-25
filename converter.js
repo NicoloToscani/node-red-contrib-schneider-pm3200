5668851fb1952a1ec36b41f4b54fc425cccbd86a
Nicolò Toscani <toscani.nicolo90@gmail.com>
2021-06-25 17:09:14 +0200

    Add files via upload

--- converter.js
+++ converter.js
@@ -10,55 +10,57 @@ module.exports = function(RED) {
             case "float32":
                 msg.payload = msg.payload.buffer.readFloatBE(0,4).toFixed(2);
                 if(msg.topic != "pFactorTot"){
-                node.send(msg);
-                } 
-            break;
-            case "uint32":
-                msg.payload = msg.payload.buffer.readUInt32BE(0,4).toFixed(2);
                 
-                // Power Factor Total: convert powerfactor into 0 to 1 range 
-                if (msg.topic === "pFactorTot") {
+                   node.send(msg);
+                
+                } 
+                // Power Factor Total: convert powerfactor into 0 to 1 range
+                else  if (msg.topic == "pFactorTot") {
+                
                     var payConv = Number(msg.payload);
+                    node.send(msg);
+                    
                     // Quad 1
                     if(payConv >= 0 && payConv <= 1){
                         msg.payload = payConv.toFixed(2);
                         msg.util = {
-        
                         loadType: "Inductive"
                         }
-                        //node.send(msg);
+                        node.send(msg);
                      }
                     // Quad 2
                     if(payConv >= -1 && payConv <= 0){
                         msg.payload = payConv.toFixed(2);
                         msg.util = {
-       
                         loadType: "Capacitive"
                         }
-                        //node.send(msg);
+                        node.send(msg);
                      }
                     // Quad 3
                     if(payConv >= -2 && payConv <= -1){
                         payConv = (-2) -payconv;
                         msg.payload = payConv.toFixed(2);
                         msg.util = {
-        
                         loadType: "Inductive"
                         }
-                        //node.send(msg);
+                        node.send(msg);
                     }
                     // Quad 4
                     if(payConv >= 1 && payConv <= 2){
                         payConv = 2 - payConv;
                         msg.payload = payConv.toFixed(2);
                         msg.util = {
-        
                         loadType: "Capacitive"
                         }
-                      //node.send(msg);
+                      node.send(msg);
                     }
                  }
                 
+                
+                
+            break;
+            case "uint32":
+                msg.payload = msg.payload.buffer.readUInt32BE(0,4).toFixed(2);
                 node.send(msg);
             break;
             case "int64":
